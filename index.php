6af06e5e274f8c12a05d89a894456d59c9e5c32d
Marcus Dalgren <marcus.dalgren@gmail.com>
2013-11-06 00:07:30 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcus Dalgren
+ * Date: 2013-11-05
+ * Time: 23:47
+ */
+require("./vendor/autoload.php");
+require("./classes/game.php");
+require("./tests/gameTest.php");
