898e7b0548810144e68487191bfde708fde47fc8
Marcus Dalgren <marcus.dalgren@gmail.com>
2013-11-07 21:23:27 +0100

    Starting bowling game

--- tests/gameTest.php
+++ tests/gameTest.php
@@ -7,6 +7,7 @@
  */
 $path = dirname(dirname(__FILE__));
 require_once($path . "/classes/game.php");
+
 class GameTest extends PHPUnit_Framework_TestCase {
 	private $g;
 
@@ -17,55 +18,17 @@ class GameTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @test
 	 */
-	public function Should_roll_gutter_game() {
-		$this->rollMany(20, 0);
-		$this->assertEquals(0, $this->g->score());
-	}
-
-	/**
-	 * @test
-	 */
-	public function Should_roll_all_ones() {
-		$this->rollMany(20, 1);
-		$this->assertEquals(20, $this->g->score());
-	}
-
-	/**
-	 * @test
-	 */
-	public function Should_roll_one_spare() {
-		$this->g->rollSpare();
-		$this->g->roll(3);
-		$this->rollMany(17, 0);
-		$this->assertEquals(16, $this->g->score());
-	}
-
-	/**
-	 * @test
-	 **/
-	public function Should_roll_one_strike() {
-		$this->rollStrike();
-		$this->g->roll(3);
-		$this->g->roll(4);
-		$this->rollMany(16, 0);
-		$this->assertEquals(24, $this->g->score());
-	}
-
-	/**
-	 * @test
-	 **/
-	public function Should_roll_perfect_game() {
-		$this->rollMany(12, 10);
-		$this->assertEquals(300, $this->g->score());
-	}
-
-	public function rollMany($n, $pins) {
-		for ($i = 0; $i < $n; $i++) {
-			$this->g->roll($pins);
+	public function testGutterGame() {
+		for ($i = 0; $i < 20; $i++) {
+			$this->g->roll(0);
 		}
+		$this->assertSame(0, $this->g->score());
 	}
 
-	public function rollStrike() {
-		$this->g->roll(10);
+	public function testGutterGameFail() {
+		for ($i = 0; $i < 20; $i++) {
+			$this->g->roll(0);
+		}
+		$this->assertEquals(0, $this->g->score());
 	}
 }
